c3beb07432a855c65098470429ead4e820b118a8
Yaroslav <khaidayev@gmail.com>
2016-03-23 23:15:04 +0600

    Created by Coding Ground Client

---
+++ parsingURL/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char url[255];
+    
+    // regExps
+    /*
+     *
+    
+     */
+    char http
+    
+    scanf("%s", &url);
+    
+    ststr(url, );
+    
+    printf("%s\n", url);
+
+    return 0;
+}
+
